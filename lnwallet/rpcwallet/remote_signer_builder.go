8312b99b9b0d9c2115533dd36e937e2a74804ad4
Viktor Tigerström <vtigerstrom@gmail.com>
2024-05-14 16:15:00 +0200

    rpcwallet: add RemoteSignerBuilder

---
+++ lnwallet/rpcwallet/remote_signer_builder.go
@@ -0,0 +1,57 @@
+package rpcwallet
+
+import (
+	"errors"
+
+	"github.com/lightningnetwork/lnd/lncfg"
+)
+
+// RemoteSignerBuilder is creates instances of the RemoteSigner interface, based
+// on the provided configuration.
+type RemoteSignerBuilder struct {
+	cfg *lncfg.RemoteSigner
+}
+
+// NewRemoteSignerBuilder creates a new instance of the RemoteSignerBuilder.
+func NewRemoteSignerBuilder(cfg *lncfg.RemoteSigner) *RemoteSignerBuilder {
+	return &RemoteSignerBuilder{cfg}
+}
+
+// Build creates a new RemoteSigner instance. If the configuration specifies
+// that an inbound remote signer should be used, a new InboundRemoteSigner is
+// created. If the configuration specifies that an outbound remote signer should
+// be used, a new OutboundRemoteSigner is created.
+// The function returns the created RemoteSigner instance, and a cleanup
+// function that should be called when the RemoteSigner is no longer needed.
+func (b *RemoteSignerBuilder) Build() (RemoteSigner, func(), error) {
+	if b.cfg == nil {
+		return nil, nil, errors.New("remote signer config is nil")
+	}
+
+	// Validate that the configuration has valid values set.
+	err := b.cfg.Validate()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Create the remote signer based on the configuration.
+	switch b.cfg.SignerType {
+	case lncfg.DefaultInboundRemoteSignerType:
+		return b.createInboundRemoteSigner()
+	default:
+		return nil, nil, errors.New("unknown remote signer type")
+	}
+}
+
+// createInboundRemoteSigner creates a new InboundRemoteSigner instance.
+// The function returns the created InboundRemoteSigner instance, and a cleanup
+// function that should be called when the InboundRemoteSigner is no longer
+// needed.
+func (b *RemoteSignerBuilder) createInboundRemoteSigner() (
+	*InboundRemoteSigner, func(), error) {
+
+	return NewInboundRemoteSigner(
+		b.cfg.RPCHost, b.cfg.TLSCertPath, b.cfg.MacaroonPath,
+		b.cfg.Timeout,
+	)
+}
